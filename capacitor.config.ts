de9463b94e7ea696fa7853fa482002f2ce6fdde6
Wilgert Velinga <wilgert@wilgert.nl>
2023-03-14 11:13:30 +0100

    change app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'nl.vattenfall.liveupdatesexample',
   appName: 'live-updates-example',
   webDir: 'www',
   bundledWebRuntime: false
